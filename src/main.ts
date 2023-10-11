7d0acf2d9434b66ff2f6be01b9cfbe88b466effe
ashley-perez <ashleypere428@gmail.com>
2023-10-10 18:00:39 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,28 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Ashley's game :D";
+let counter: number = 0;
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// creating the button
+const button = document.createElement("button");
+button.type = "button";
+button.innerText = "🦈";
+
+// adding the counter
+const showCounter = document.createElement("div");
+showCounter.innerHTML = `${counter} Shark Food (he hungy)`;
+app.append(showCounter);
+
+button.addEventListener("mousedown", () => {
+  console.log("click registered");
+  counter++;
+  showCounter.innerHTML = `${counter} Shark Food (he hungy)`;
+});
+
+app.append(button);
