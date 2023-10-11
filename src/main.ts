99ea49427961c6c6a298824ff70357067fae3b66
ashley-perez <ashleypere428@gmail.com>
2023-10-10 20:25:01 -0700

    step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Feed sharky";
 let counter: number = 0.0;
 let currentTimestamp = performance.now();
+let growthrate: number = 0;
 
 document.title = gameName;
 
@@ -17,21 +18,33 @@ const button = document.createElement("button");
 button.type = "button";
 button.innerText = "🦈";
 
+// creating a new purchasable item button
+const firstUpgradeButton = document.createElement("button");
+firstUpgradeButton.className = "upgrade-button";
+// firstUpgradeButton.type = "button"
+firstUpgradeButton.innerText = "🦐 not filling";
+
+firstUpgradeButton.addEventListener("click", () => {
+  if (counter >= 10) {
+    counter -= 10;
+    growthrate += 1 / 50;
+
+    showCounter.innerHTML = `${counter.toFixed(2)} Shark Food (he hungy)`;
+    firstUpgradeButton.innerText = "🦐 ok...";
+
+    firstUpgradeButton.disabled = counter < 10;
+  }
+});
+
 // adding the counter
 const showCounter = document.createElement("div");
 showCounter.innerHTML = `${counter} Shark Food (he hungy)`;
 app.append(showCounter);
 
-// adding the interval
-// setInterval(() => {
-//   counter++;
-//   showCounter.innerHTML = `${counter} Shark Food (he hungy)`;
-// }, 1000);
-
 function updateCounter() {
   const nowTime = performance.now();
   const deltaTime = (nowTime - currentTimestamp) / 1000;
-  counter += deltaTime;
+  counter += deltaTime * growthrate;
   showCounter.innerHTML = `${counter.toFixed(2)} Shark Food (he hungy)`;
 
   currentTimestamp = nowTime;
@@ -45,5 +58,6 @@ button.addEventListener("mousedown", () => {
 });
 
 app.append(button);
+app.append(firstUpgradeButton);
 
 requestAnimationFrame(updateCounter);
