0582a9107eef9f49ec0de5daf8057fc44dcab7e3
ashley-perez <ashleyperez428@gmail.com>
2023-10-13 10:28:12 -0700

    step 10 complete

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,6 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// creating the main button
 const button = document.createElement("button");
 button.type = "button";
 button.innerText = "🦈";
@@ -31,13 +30,46 @@ interface Item {
   name: string;
   cost: number;
   rate: number;
-  count?: number;
+  count: number;
+  description: string;
 }
 
 const availableItems: Item[] = [
-  { name: "🌿 kelp... ", cost: 10, rate: 0.1, count: 0 },
-  { name: "🦐 shrimpy :| ", cost: 100, rate: 2.0, count: 0 },
-  { name: "🐟 fish :) ", cost: 1000, rate: 50.0, count: 0 },
+  {
+    name: "🌿 kelp... ",
+    cost: 10,
+    rate: 0.1,
+    count: 0,
+    description: "vegan moment",
+  },
+  {
+    name: "🦐 shrimpy :| ",
+    cost: 100,
+    rate: 2.0,
+    count: 0,
+    description: "he's watching his figure",
+  },
+  {
+    name: "🐟 fish :) ",
+    cost: 1000,
+    rate: 50.0,
+    count: 0,
+    description: "ok, out of the trenches now",
+  },
+  {
+    name: "🦭 seal :0 ",
+    cost: 5000,
+    rate: 100.0,
+    count: 0,
+    description: "lots of nutritional value",
+  },
+  {
+    name: "🧍‍♂️ man :D ",
+    cost: 15000,
+    rate: 500.0,
+    count: 0,
+    description: "'I USED TO PRAY FOR TIMES LIKE THIS'",
+  },
 ];
 
 availableItems.forEach((item) => {
@@ -46,10 +78,15 @@ availableItems.forEach((item) => {
   upgradeButton.innerText = `${item.name}: Buy for ${item.cost} braincells`;
   upgradeButton.className = "upgrade-button";
 
+  const upgradeDescription = document.createElement("div");
+  upgradeDescription.innerText = item.description;
+
   const upgradeCountDisplay = document.createElement("div");
+
   upgradeCountDisplay.innerHTML = `${item.name}: ${item.count} purchased`;
 
   app.append(upgradeButton);
+  app.append(upgradeDescription);
   app.append(upgradeCountDisplay);
 
   upgradeButton.addEventListener("mousedown", () => {
@@ -74,7 +111,6 @@ function updateCounter() {
   const nowTime = performance.now();
   const deltaTime = (nowTime - currentTimestamp) / 1000;
   counter += deltaTime * growthrate;
-  // showCounter.innerHTML = `${counter.toFixed(2)} Shark Food (he hungy)`;
   if (counter >= 1000 && counter <= 3000) {
     showCounter.innerHTML = `${counter.toFixed(2)} Shark Food (ok...)`;
   } else if (counter >= 3000 && counter <= 5000) {
