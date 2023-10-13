e089329acb392749b038d209c26baf73fd726844
ashley-perez <ashleypere428@gmail.com>
2023-10-12 21:26:24 -0700

    Step 6 complete - too many braincells used

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Feed sharky";
 let counter: number = 0.0;
 let currentTimestamp = performance.now();
-let growthrate: number = 0;
+let growthrate: number = 0.0;
 
 document.title = gameName;
 
@@ -13,40 +13,55 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// creating the button
+// creating the main button
 const button = document.createElement("button");
 button.type = "button";
 button.innerText = "🦈";
+app.append(button);
 
-// creating a new purchasable item button
-const firstUpgradeButton = document.createElement("button");
-firstUpgradeButton.className = "upgrade-button";
-// firstUpgradeButton.type = "button"
-firstUpgradeButton.innerText = "🦐 not filling";
-
-firstUpgradeButton.addEventListener("click", () => {
-  if (counter >= 10) {
-    counter -= 10;
-    growthrate += 1;
-
-    showCounter.innerHTML = `${Math.floor(counter)} Shark Food (he hungy)`;
-    firstUpgradeButton.innerText = "🦐 ok...";
-
-    firstUpgradeButton.disabled = counter < 10;
-  }
-});
-
-// adding the counter
 const showCounter = document.createElement("div");
 showCounter.innerHTML = `${counter} Shark Food (he hungy)`;
 app.append(showCounter);
+
+const growthRateDisplay = document.createElement("div");
+growthRateDisplay.innerHTML = `${growthrate.toFixed(2)} food/sec`;
+app.append(growthRateDisplay);
+
+const upgrades = [
+  { id: "🌿 kelp... ", cost: 10, rate: 0.1, count: 0 },
+  { id: "🦐 shrimpy :| ", cost: 100, rate: 2.0, count: 0 },
+  { id: "🐟 fish :) ", cost: 1000, rate: 50.0, count: 0 },
+];
+
+upgrades.forEach((upgrade) => {
+  const upgradeButton = document.createElement("button");
+  upgradeButton.type = "button";
+  upgradeButton.innerText = `${upgrade.id}: Buy for ${upgrade.cost} braincells`;
+  upgradeButton.className = "upgrade-button";
+
+  const upgradeCountDisplay = document.createElement("div");
+  upgradeCountDisplay.innerHTML = `${upgrade.id}: ${upgrade.count} purchased`;
+
+  app.append(upgradeButton);
+  app.append(upgradeCountDisplay);
+
+  upgradeButton.addEventListener("mousedown", () => {
+    if (counter >= upgrade.cost) {
+      counter -= upgrade.cost;
+      upgrade.count++;
+      growthrate += upgrade.rate;
+      showCounter.innerHTML = `${counter.toFixed(2)} Shark Food (he hungy)`;
+      upgradeCountDisplay.innerHTML = `${upgrade.id}: ${upgrade.count} purchased`;
+      growthRateDisplay.innerHTML = `${growthrate.toFixed(2)} units/sec`;
+    }
+  });
+});
 
 function updateCounter() {
   const nowTime = performance.now();
   const deltaTime = (nowTime - currentTimestamp) / 1000;
   counter += deltaTime * growthrate;
-  showCounter.innerHTML = `${Math.floor(counter)} Shark Food (he hungy)`;
-
+  showCounter.innerHTML = `${counter.toFixed(2)} Shark Food (he hungy)`;
   currentTimestamp = nowTime;
   requestAnimationFrame(updateCounter);
 }
@@ -54,10 +69,7 @@ function updateCounter() {
 button.addEventListener("mousedown", () => {
   console.log("click registered");
   counter++;
-  showCounter.innerHTML = `${Math.floor(counter)} Shark Food (he hungy)`;
+  showCounter.innerHTML = `${counter.toFixed(2)} Shark Food (he hungy)`;
 });
-
-app.append(button);
-app.append(firstUpgradeButton);
 
 requestAnimationFrame(updateCounter);
